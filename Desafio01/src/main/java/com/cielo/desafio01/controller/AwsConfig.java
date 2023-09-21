e3f1164de3dfc79bb20b37b594d06286c9430369
Anderson de Aguiar de Oliveira <90530503+andersonaoliveira@users.noreply.github.com>
2023-09-21 01:43:36 -0300

    Create AwsConfig.java

---
+++ Desafio01/src/main/java/com/cielo/desafio01/controller/AwsConfig.java
@@ -0,0 +1,15 @@
+package com.cielo.desafio01.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+
+@Configuration
+public class AwsConfig {
+
+    @Bean
+    public AmazonSQS amazonSQS() {
+        return AmazonSQSClientBuilder.defaultClient();
+    }
+}
